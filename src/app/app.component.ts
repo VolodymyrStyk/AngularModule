6db14eb56fc7fb30bec6036d6909c083c12f837f
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-05-27 14:13:56 +0300

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  greeting: string = 'Hello!';
+  greeting: string = 'Hello !!!';
   users: string = 'Users';
   posts: string = 'Posts';
   albums: string = 'Albums';
