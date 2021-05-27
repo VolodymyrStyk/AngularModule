6f18205e70a0c09d00bc234d8ac48bc953335ca9
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-05-28 00:02:53 +0300

    update project

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,12 +7,13 @@ import {User} from "../models/User";
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  @Input()
-  user: User;
+
 
   constructor() {
   }
 
   ngOnInit(): void {
   }
+  @Input()
+  user: User;
 }
