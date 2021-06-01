6fa829f541fe570242a7a335befdddb2632d4393
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-01 16:44:05 +0300

    Finish lection 2

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -11,9 +11,6 @@ import {ActivatedRoute} from "@angular/router";
 export class CommentsComponent implements OnInit {
   comments: Comments[];
 
-  // constructor(private commentService: CommentService) {
-  //   this.commentService.getComment().subscribe(value => this.comments = value);
-  // }
   constructor(private activatedRoute: ActivatedRoute) {
     this.activatedRoute.data.subscribe(value => this.comments = value.data)
   }
