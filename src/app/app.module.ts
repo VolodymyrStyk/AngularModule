e0e514cbf319721937e32c2a5c67ec16be3f77dc
VolodymyrStyk <volodymyr.styk@gmail.com>
2021-06-03 00:12:02 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {UsersComponent} from './components/users/users.component';
-import {UserComponent} from './components/user/user.component';
 import {HttpClientModule} from "@angular/common/http";
-import {RouterModule, Routes} from "@angular/router";
-import {HomeComponent} from './components/home/home.component';
-import {UserDetailsComponent} from './components/user-details/user-details.component';
-
-let routes:Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: 'users', component: UsersComponent,
-    children: [{
-      path: ':id', component: UserDetailsComponent,
-    }]
-  }
-]
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    HomeComponent,
-    UserDetailsComponent,
-
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
